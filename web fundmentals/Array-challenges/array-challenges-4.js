67cf66b45368828b90b8863f75e6952a4a549f3d
Hosni2005 <hosninouraldin@gmail.com>
2026-04-22 15:23:01 +0300

    array-challenge-4

---
+++ web fundmentals/Array-challenges/array-challenges-4.js
@@ -0,0 +1,12 @@
+function reverse(arr) {
+    for (let i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+// Test
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result);

338d0fdd1a57ea13c4aac1125f89cf16124140a5
Hosni2005 <hosninouraldin@gmail.com>
2026-07-27 13:41:04 +0300

    first JSX

---
+++ Mern-Stack/React/Fisrt_JSX/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+  <div>
+    <h1>Hello Dojo!</h1>
+    <h3>Things I need to do:</h3>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+  </div>
+  );
+}
+
+export default App

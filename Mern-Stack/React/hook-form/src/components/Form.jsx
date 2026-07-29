12c2a456787de55af9b8d86f8b1d9c5c83bed9fa
Hosni2005 <hosninouraldin@gmail.com>
2026-07-29 16:20:37 +0300

    More Forms & Hook Form

--- Mern-Stack/React/hook-form/src/components/Form.jsx
+++ Mern-Stack/React/hook-form/src/components/Form.jsx
@@ -3,10 +3,19 @@ import { useState } from "react";
 
 const Form = () => {
   const [firstName, setfirstName] = useState("");
+  const [firstNameErorr, setfirstNameErorr] = useState("");
+
   const [lastName, setlastName] = useState("");
+  const [lastNameErorr, setlastNameErorr] = useState("");
+
   const [Email, setEmail] = useState("");
+  const [EmailErorr, setEmailErorr] = useState("");
+
   const [Password, setPassword] = useState("");
+  const [PasswordErorr, setPasswordErorr] = useState("");
+
   const [confirmPassword, setconfirmPassword] = useState("");
+  const [confirmPasswordErorr, setconfirmPasswordErorr] = useState("");
 
   return (
     <form>
@@ -17,9 +26,20 @@ const Form = () => {
           name="firstName"
           id="firstName"
           value={firstName}
-          onChange={(e) => setfirstName(e.target.value)}
+          onChange={(e) => {
+            setfirstName(e.target.value);
+
+            if (e.target.value.length <= 2) {
+              setfirstNameErorr("Must be longer than 2 characters");
+            } else {
+              setfirstNameErorr("");
+            }
+          }}
         />
+
+        <p>{firstNameErorr}</p>
       </div>
+
       <div>
         <label htmlFor="lastName">lastName</label>
         <input
@@ -27,9 +47,19 @@ const Form = () => {
           name="lastName"
           id="lastName"
           value={lastName}
-          onChange={(e) => setlastName(e.target.value)}
+          onChange={(e) => {
+            setlastName(e.target.value);
+
+            if (e.target.value.length <= 2) {
+              setlastNameErorr("Must be longer than 2 characters");
+            } else {
+              setlastNameErorr("");
+            }
+          }}
         />
+        <p>{lastNameErorr}</p>
       </div>
+
       <div>
         <label htmlFor="Email">Email</label>
         <input
@@ -37,9 +67,19 @@ const Form = () => {
           name="Email"
           id="Email"
           value={Email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+
+            if (e.target.value.length <= 2) {
+              setEmailErorr("Must be longer than 2 characters");
+            } else {
+              setEmailErorr("");
+            }
+          }}
         />
+        <p>{EmailErorr}</p>
       </div>
+
       <div>
         <label htmlFor="Password">Password</label>
         <input
@@ -47,18 +87,38 @@ const Form = () => {
           name="Password"
           id="Password"
           value={Password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+
+            if (e.target.value.length <= 8) {
+              setPasswordErorr("Must be longer than 8 characters");
+            } else {
+              setPasswordErorr("");
+            }
+          }}
         />
+        <p>{PasswordErorr}</p>
       </div>
+
       <div>
         <label htmlFor="confirmPassword">confirmPassword</label>
+
         <input
           type="password"
           name="confirmPassword"
           id="confirmPassword"
           value={confirmPassword}
-          onChange={(e) => setconfirmPassword(e.target.value)}
+          onChange={(e) => {
+            setconfirmPassword(e.target.value);
+
+            if (e.target.value.length <= 8) {
+              setconfirmPasswordErorr("Must be longer than 8 characters");
+            } else {
+              setconfirmPasswordErorr("");
+            }
+          }}
         />
+        <p>{confirmPasswordErorr}</p>
       </div>
 
       <hr />
